ff4248da2c57a105c663c7c6f641f9927f8417b3
Wel <wel80@list.ru>
2019-08-12 18:44:46 +0700

    Пишем первую функцию

--- index.php
+++ index.php
@@ -50,6 +50,16 @@ $advertising_list = [
 ];
 
 $user_name = 'Валерий'; // укажите здесь ваше имя
+
+function price_format($cost) {
+    $cost_ceil = ceil($cost);
+    if ($cost_ceil >= 1000) {
+        $cost_format = number_format($cost_ceil, 0, ',', ' ');
+    } else {
+        $cost_format = $cost_ceil;
+    }
+    return ($cost_format . ' <b class="rub">р</b>');
+}
 ?>
 
 <!DOCTYPE html>
@@ -105,7 +115,7 @@ $user_name = 'Валерий'; // укажите здесь ваше имя
             <!--заполните этот список из массива категорий-->
             <?php foreach ($category_list as $key => $val) { ?>
                 <li class="promo__item promo__item--boards">
-                    <a class="promo__link" href="pages/all-lots.html"><?php print($val); ?></a>
+                    <a class="promo__link" href="pages/all-lots.html"><?=$val; ?></a>
                 </li>
             <?php }; ?>
         </ul>
@@ -119,15 +129,15 @@ $user_name = 'Валерий'; // укажите здесь ваше имя
             <?php foreach ($advertising_list as $key => $val) { ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src="<?php print($val["image"]); ?>" width="350" height="260" alt="<?php print($val["name"]); ?>">
+                        <img src="<?=$val["image"]; ?>" width="350" height="260" alt="<?=$val["name"]; ?>">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?php print($val["category"]); ?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?php print($val["name"]); ?></a></h3>
+                        <span class="lot__category"><?=$val["category"]; ?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$val["name"]; ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?php print($val["price"]); ?><b class="rub">р</b></span>
+                                <span class="lot__cost"><?=price_format($val["price"]); ?></span>
                             </div>
                             <div class="lot__timer timer">
                                 12:23
@@ -147,7 +157,7 @@ $user_name = 'Валерий'; // укажите здесь ваше имя
             <!--заполните этот список из массива категорий-->
             <?php foreach ($category_list as $key => $val) { ?>
                 <li class="nav__item">
-                    <a href="pages/all-lots.html"><?php print($val); ?></a>
+                    <a href="pages/all-lots.html"><?=$val; ?></a>
                 </li>
             <?php }; ?>
         </ul>
