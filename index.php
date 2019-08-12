17d39b95ef10ff46e799efc7064f731b02d0a5c6
Wel <wel80@list.ru>
2019-08-12 18:54:40 +0700

    Убрал print в имени

--- index.php
+++ index.php
@@ -89,7 +89,7 @@ function price_format($cost) {
         <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
             <?php if ($is_auth == 1) { ?>
                 <div class="user-menu__logged">
-                    <p><?php print($user_name); ?></p>
+                    <p><?=$user_name; ?></p>
                     <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
                     <a class="user-menu__logout" href="#">Выход</a>
                 </div>
