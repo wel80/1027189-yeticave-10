c24325ab1ca39748cbe15b51a2519f302b9cce67
Wel <wel80@list.ru>
2019-08-20 23:20:20 +0700

    Добавил сортировку по лотам, что бы получить новые

--- request.sql
+++ request.sql
@@ -5,10 +5,13 @@ SELECT name_cat FROM category;
 
 /*Получить самые новые, открытые лоты. 
 Каждый лот должен включать название, стартовую цену, ссылку на изображение, цену, название категории*/
-SELECT name_lot, initial_price, image_lot, name_cat
+SELECT name_lot, initial_price, image_lot, MAX(bet_amount), name_cat
 FROM lot
-JOIN category ON cat_id = id_cat
-WHERE completion_date > NOW();
+LEFT JOIN category ON cat_id = id_cat
+LEFT JOIN rate ON id_lot = lot_id
+WHERE completion_date > NOW()
+GROUP BY id_lot
+ORDER BY date_lot ASC;
 
 /*Показать лот по его id. Получите также название категории, к которой принадлежит лот*/
 SELECT name_lot, image_lot, name_cat
