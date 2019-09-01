f68d625fc5b2df451374fadfe5966106ada7b0ad
Wel <wel80@list.ru>
2019-09-01 10:47:02 +0700

    Валидация формы

--- functions.php
+++ functions.php
@@ -24,4 +24,55 @@ function rest_time ($date_end) {
         str_pad($period_min, 2, "0", STR_PAD_LEFT)
     ];
     return $time_expiry;
-}
+};
+
+function include_template_error($customer, $auth_rand) {
+    $layout_content = include_template('error.php', [
+        'main_content' => 'Ошибка запроса на получение информации из базы данных',
+        'user_name' => $customer,
+        'title' => 'Ошибка',
+        'is_auth' => $auth_rand
+    ]);
+    exit($layout_content);
+};
+
+function getPostVal($name) {
+    return $_POST[$name] ?? ""; 
+};
+
+function validateCategory($name, $allowed_list) {
+    $category_name = $_POST[$name];
+
+    if (!in_array($category_name, $allowed_list)) {
+        return "Указана несуществующая категория";
+    };
+    return null;
+};
+
+function validateLength($name, $min, $max) {
+    $len = strlen($_POST[$name]);
+
+    if ($len < $min or $len > $max) {
+        return "Значение должно быть от $min до $max символов";
+    };
+    return null;
+};
+
+function validateNumber($name) {
+    $number = filter_var($_POST[$name], FILTER_VALIDATE_INT);
+    if ($number < 1) {
+        return "Укажите целое число больше нуля";
+    };
+    return null;
+};
+
+function validateDate($name) {
+    $date_format = date_create_from_format('Y-m-d', $_POST[$name]);
+    $date_end_unix = strtotime($_POST[$name]);
+    $date_current_unix = time();
+    $period_unix = $date_end_unix - $date_current_unix;
+    if ($date_format && $period_unix > 86400) {
+        return null;
+    };
+    return "Введите дату в формате ГГГГ-ММ-ДД";
+};
--- lot.php
+++ lot.php
@@ -1,6 +1,6 @@
 <?php
-
 require_once('init.php');
+
 
 if (empty($_GET['id'])) {
   exit(http_response_code(404));
@@ -10,7 +10,9 @@ $id_get = filter_var($_GET['id'], FILTER_VALIDATE_INT);
 if (!$id_get) {
   exit(http_response_code(404));
 };
+
 $id = intval($id_get);
+
 
 $query_lot = 'SELECT name_lot AS "name", name_cat AS "category", description_lot, 
 initial_price AS "price", initial_price + step_rate AS "first_rate", MAX(bet_amount) AS "price_rate", 
@@ -23,34 +25,25 @@ GROUP BY id_lot';
 $result_lot = mysqli_query($link, $query_lot);
 
 if ($result_lot === false) {
-  $layout_content = include_template('error.php', [
-    'main_content' => 'Ошибка запроса на получение информации из базы данных',
-    'user_name' => $user_name,
-    'title' => 'Ошибка',
-    'is_auth' => $is_auth
-  ]);
-  exit($layout_content);
+  include_template_error($user_name, $is_auth);
 };
 
 $is_lot = mysqli_fetch_assoc($result_lot);
+
 if (!$is_lot) {
   exit(http_response_code(404));
-}
+};
+
 
 $query_category_list = 'SELECT name_cat, code_cat FROM category';
 $result_category_list = mysqli_query($link, $query_category_list);
 
 if ($result_category_list === false) {
-  $layout_content = include_template('error.php', [
-    'main_content' => 'Ошибка запроса на получение информации из базы данных',
-    'user_name' => $user_name,
-    'title' => 'Ошибка',
-    'is_auth' => $is_auth
-  ]);
-  exit($layout_content);
+    include_template_error($user_name, $is_auth);
 };
 
 $all_category = mysqli_fetch_all($result_category_list, MYSQLI_ASSOC);
+
 
 $main_content = include_template('main-lot.php', [
   'category_list' => $all_category,
@@ -63,6 +56,6 @@ $layout_content = include_template('layout.php', [
   'title' => $is_lot['name'],
   'category_list' => $all_category,
   'is_auth' => $is_auth
-]);;
+]);
 
 print($layout_content);
